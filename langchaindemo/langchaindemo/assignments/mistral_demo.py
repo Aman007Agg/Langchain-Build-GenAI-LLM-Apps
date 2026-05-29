96cee321277ea21541c24f4b6e4e8d7bfa5c7d0d
Aman007Agg <G24ait172@iitj.ac.in>
2026-05-29 17:46:32 +0530

    Initial commit- Langchain Concepts

---
+++ langchaindemo/langchaindemo/assignments/mistral_demo.py
@@ -0,0 +1,9 @@
+import os
+# from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
+
+llm=ChatOllama(model="mistral")
+
+question = input("Enter the question")
+response = llm.invoke(question)
+print(response.content)
---
+++ langchaindemo/langchaindemo/basics/gemma_demo.py
@@ -0,0 +1,10 @@
+import os
+# from langchain_community.chat_models import ChatOllama
+
+from langchain_ollama import ChatOllama
+
+llm=ChatOllama(model="gemma:2b")
+
+question = input("Enter the question")
+response = llm.invoke(question)
+print(response.content)
