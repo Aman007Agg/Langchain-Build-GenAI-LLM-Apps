f8d60ba67d176b945221b7950dcef09692f3518f
Aman007Agg <G24ait172@iitj.ac.in>
2026-05-30 20:49:29 +0530

    Embeddings-into demo

--- langchaindemo/langchaindemo/embeddings/embeddings_demo.py
+++ langchaindemo/langchaindemo/embeddings/embeddings_demo.py
@@ -1,8 +1,10 @@
 import os
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-llm=OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# llm=OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+llm = OllamaEmbeddings(model="llama3.2")
 
 text = input("Enter the text")
 response = llm.embed_query(text)
